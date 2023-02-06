91ad1de71b31e6c9bb7bbe7e4be04aa8f9bd57af
João Luiz <jleaopitta@gmail.com>
2023-02-06 12:11:47 -0300

    refactor: added react-query to maintain a server state and better app flow

--- ecommerce/src/Root.tsx
+++ ecommerce/src/Root.tsx
@@ -1,17 +1,21 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'styled-components'
 import { App } from './App'
 import { CartProvider } from './context/CartContext'
+import { queryClient } from './services'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/theme'
 
 export function Root () {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <CartProvider>
-        <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={defaultTheme}>
+        <CartProvider>
+          <App />
 
-        <GlobalStyle />
-      </CartProvider>
-    </ThemeProvider>
+          <GlobalStyle />
+        </CartProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
--- ecommerce/src/pages/Home/Home.tsx
+++ ecommerce/src/pages/Home/Home.tsx
@@ -1,44 +1,25 @@
-import { useEffect, useState } from 'react'
-import { toast } from 'react-hot-toast'
+import { useQuery } from '@tanstack/react-query'
 import { getMovies, TProduct } from '../../services'
 import { Loading, MovieCard } from '../../ui'
 
 import * as S from './styles'
 
 export function Home () {
-  const [IsLoading, setIsLoading] = useState(true)
-  const [products, setProducts] = useState<TProduct[]>([])
+  const { data, isLoading } = useQuery(['products'], getMovies)
 
-  async function fetchProducts () {
-    try {
-      setIsLoading(true)
-      const movies = await getMovies()
-
-      if (movies) {
-        const spiderman = movies.splice(2, 1)
-        movies.splice(1, 0, spiderman[0])
-        setProducts(movies)
-      }
-    } catch (error) {
-      toast.error('Ops! Não conseguimos trazer a lista de produtos, por favor contate um administrador!')
-    } finally {
-      setIsLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchProducts()
-  }, [])
+  const newOrderData: TProduct[] = structuredClone(data)
+  const spiderman = newOrderData?.splice(2, 1)
+  newOrderData?.splice(1, 0, spiderman[0])
 
   return (
     <>
-      {IsLoading
+      {isLoading
         ? (
           <Loading />
           )
         : (
           <S.ProductsContainer>
-            {products.map(product => (
+            {newOrderData?.map(product => (
               <MovieCard key={product.id} product={product} />
             ))}
           </S.ProductsContainer>
